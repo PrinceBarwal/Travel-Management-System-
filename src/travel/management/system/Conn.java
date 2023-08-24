033c2ddecefa3f4eefb4da44dc6c81aa68d30292
Prince Saini <princesaini0144@gmail.com>
2023-08-24 16:13:17 +0530

    add connection file

--- src/travel/management/system/Conn.java
+++ src/travel/management/system/Conn.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package travel.management.system;
-
-/**
- *
- * @author princ
- */
+import java.sql.*;
 public class Conn {
-    
+    Conn(){
+        Connection c;
+        Statement s;
+        try{
+            c = DriverManager.getConnection("jdbc://mysql://localhost:3306/travelmanagementsystem" , "root" , "saini@786;");
+            s = c.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
